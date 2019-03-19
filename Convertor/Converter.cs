e5b7f7323723dfeb848e47574d6c7b67c596b3da
wismark <trustworthy.wismark@gmail.com>
2019-03-19 10:23:35 +0200

    Add project files.

---
+++ Convertor/Converter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Convertor
+{
+    public static class Converter
+    {
+        public static bool Convert(string fileInputName, string fileOutputName, Encoding inEncoding, Encoding outEncoding, bool overwrite = false)
+        {
+            try
+            {
+                if (File.Exists(fileInputName + ".txt"))
+                {
+                    using (StreamReader sr = new StreamReader(fileInputName + ".txt", inEncoding))
+                    {
+                        if (!overwrite) fileOutputName = GetUniqueName(fileOutputName);
+
+                        using (StreamWriter sw = new StreamWriter(fileOutputName + ".txt", false, outEncoding))
+                        {
+                            while (!sr.EndOfStream)
+                            {
+                                FileInfo fileInfo = new FileInfo(fileInputName + ".txt");
+                                var bufferSize = Math.Min(1024 * 1024, fileInfo.Length - 3);
+                                char[] buffer = new char[bufferSize];
+                                sr.ReadBlock(buffer, 0, (int) bufferSize);
+                                sw.Write(buffer);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static string GetUniqueName(string name)
+        {
+            Int32 i = 1;
+            string temp = name + $" ({i})";
+            if (!File.Exists(name +".txt")) return name;
+            
+            while (File.Exists(temp + ".txt"))
+            {
+                i++;
+                temp = name + $" ({i})";
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ Convertor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Convertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+               var strParams = new string[4]; 
+
+                for (var i = 0; i < args.Length; i++)
+                {
+                    if (args[i] == "-i") strParams[0] = args[i + 1];
+                    if (args[i] == "-o") strParams[1] = args[i + 1];
+                    if (args[i] == "-if") strParams[2] = args[i + 1];
+                    if (args[i] == "-of") strParams[3] = args[i + 1];
+                }
+
+               var inEncoding = Encoding.GetEncoding(Int32.Parse(strParams[0]));
+               var outEncoding = Encoding.GetEncoding(Int32.Parse(strParams[1]));
+               var success = Converter.Convert(strParams[2], strParams[3], inEncoding, outEncoding);
+
+               Console.WriteLine(success ? "Success" : "Fail");
+           }
+           catch (Exception e)
+           {
+               Console.WriteLine(e);
+           }          
+        }
+    }
+
+}
